a70433f8fefd5b04893c75626fc13f47c7859d9b
fclaude <fclaude@cs.uwaterloo.ca>
2010-10-19 23:11:47 +0000

    new version, better documentation
    git-svn-id: http://svn.recoded.cl/libcds@18 41656f23-c745-496b-8a1c-052032c6b90f

---
+++ tags/libcds-v1.0.3/src/static/mapper/MapperCont.cpp
@@ -0,0 +1,88 @@
+/* alphabet_mapper_cont.cpp
+ * Copyright (C) 2008, Francisco Claude, all rights reserved.
+ *
+ * alphabet_mapper_cont definition
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <MapperCont.h>
+
+namespace cds_static
+{
+
+    MapperCont::MapperCont(const Array & seq, const BitSequenceBuilder & bmb) {
+        BitString bs(seq.getMax()+1);
+        for(size_t i=0;i<seq.getLength();i++)
+            bs.setBit(seq[i]);
+        m = bmb.build(bs);
+    }
+
+    MapperCont::MapperCont(const uint * A, const size_t len, const BitSequenceBuilder & bmb) {
+        uint max_v = 0;
+        for(uint i=0;i<len;i++)
+            max_v = max(max_v,A[i]);
+        max_v++;
+
+        BitString bs(max_v);
+        for(size_t i=0;i<len;i++)
+            bs.setBit(A[i]);
+        m = bmb.build(bs);
+    }
+
+    MapperCont::MapperCont() {
+    }
+
+    MapperCont::~MapperCont() {
+        delete m;
+    }
+
+    uint MapperCont::map(uint s) const
+    {
+        return m->rank1(s);
+    }
+
+    uint MapperCont::unmap(uint s) const
+    {
+        return m->select1(s);
+    }
+
+    size_t MapperCont::getSize() const
+    {
+        return sizeof(MapperCont)+m->getSize();
+    }
+
+    void MapperCont::save(ofstream & out) const
+    {
+        assert(out.good());
+        uint wr = MAPPER_CONT_HDR;
+        saveValue(out,wr);
+        m->save(out);
+    }
+
+    MapperCont * MapperCont::load(ifstream & input) {
+        assert(input.good());
+        uint rd = loadValue<uint>(input);
+        if(rd!=MAPPER_CONT_HDR) return NULL;
+        MapperCont * ret = new MapperCont();
+        ret->m = BitSequence::load(input);
+        if(ret->m==NULL) {
+            delete ret;
+            return NULL;
+        }
+        return ret;
+    }
+};
---
+++ tags/libcds-v1.0.3/src/static/sequence/wt_node.h
@@ -0,0 +1,59 @@
+/* wt_node.h
+ * Copyright (C) 2008, Francisco Claude, all rights reserved.
+ *
+ * wt_node
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef wt_node_h
+#define wt_node_h
+
+#include <libcdsBasics.h>
+#include <wt_coder.h>
+#include <vector>
+
+using namespace cds_utils;
+
+namespace cds_static
+{
+
+    #define WT_NODE_NULL_HDR 0
+    #define WT_NODE_INTERNAL_HDR 2
+    #define WT_NODE_LEAF_HDR 3
+
+    /** Base clase for nodes in the wavelet tree
+     *
+     *  @author Francisco Claude
+     */
+    class wt_node
+    {
+        public:
+            virtual ~wt_node() {}
+            virtual size_t rank(uint symbol, size_t pos, uint l, wt_coder * c) const = 0;
+            virtual size_t select(uint symbol, size_t pos, uint l, wt_coder * c) const = 0;
+            virtual uint access(size_t pos) const = 0;
+            virtual uint access(size_t pos, size_t & rankp) const = 0;
+            virtual size_t getSize() const = 0;
+            virtual void save(ofstream & fp) const = 0;
+            static wt_node * load(ifstream & fp);
+    };
+
+};
+
+#include <wt_node_internal.h>
+#include <wt_node_leaf.h>
+#endif
