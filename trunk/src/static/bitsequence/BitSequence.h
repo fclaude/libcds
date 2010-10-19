15230659f9618f2b65de18314f1b16fb1f36a63e
fclaude <fclaude@cs.uwaterloo.ca>
2010-10-19 03:59:43 +0000

    fixing documentation
    git-svn-id: http://svn.recoded.cl/libcds@17 41656f23-c745-496b-8a1c-052032c6b90f

--- trunk/src/static/bitsequence/BitSequence.h
+++ trunk/src/static/bitsequence/BitSequence.h
@@ -55,19 +55,38 @@ namespace cds_static
             virtual size_t rank0(const size_t i) const;
 
             /** Returns the position of the i-th zero
-             * @return (uint)-1 if i=0, len if i>num_zeros or the position */
+             * @return (size_t)-1 if i=0, len if i>num_zeros or the position */
             virtual size_t select0(const size_t i) const;
 
             /** Returns the number of ones until position i */
             virtual size_t rank1(const size_t i) const;
 
             /** Returns the position of the i-th one
-             * @return (uint)-1 if i=0, len if i>num_ones or the position */
+             * @return (size_t)-1 if i=0, len if i>num_ones or the position */
             virtual size_t select1(const size_t i) const;
 
+            /** Return the first position starting at i that contains a 1.
+             * In case there are no more ones in the bitsequence, the function 
+             * returns the length of the bitmap 
+             */
             virtual size_t selectNext1(const size_t i) const;
+            
+            /** Return the first position starting at i that contains a 0.
+             * In case there are no more zeros in the bitsequence, the function 
+             * returns the length of the bitmap 
+             */
             virtual size_t selectNext0(const size_t i) const;
+
+            /** Return the first position starting at i moving to the left that contains a 1.
+             * In case there are no more ones to the left in the bitsequence, the function 
+             * returns (size_t)-1. 
+             */
             virtual size_t selectPrev1(const size_t i) const;
+
+            /** Return the first position starting at i moving to the left that contains a 0.
+             * In case there are no more zeros to the left in the bitsequence, the function 
+             * returns (size_t)-1. 
+             */
             virtual size_t selectPrev0(const size_t i) const;
 
             /** Returns the i-th bit */
